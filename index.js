99c1b7b17eab6b8ff98c8a30613154c0478d019b
Matheus Alves <theuves@outlook.com>
2017-06-17 21:04:38 -0400

    algumas alterações

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 module.exports = function (celsius) {
-    if (typeof celsius === 'number') {
-        return celsius * 1.8 + 32;
-    }
+  if (typeof celsius === "number") {
+    return celsius * 1.8 + 32;
+  }
 };
